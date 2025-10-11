36e21b686a83c348e5efa1a5491a246d57c8d93f
nathan-au <im.nathanau@gmail.com>
2025-10-11 13:17:54 -0400

    create client function

--- main.py
+++ main.py
@@ -2,29 +2,30 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from enum import Enum
 from datetime import datetime
-from sqlmodel import SQLModel, Field
 
 app = FastAPI()
 
+clients = []
 
 class ComplexityEnum(str, Enum):
     simple = "simple"
     average = "average"
     complex = "complex"
 
-class Client(SQLModel, table=True):
-    id: int = Field(default=None, primary_key=True)
+class Client(BaseModel):
+    id: int
     name: str
     email: str
     complexity: ComplexityEnum
-    created_at: datetime = Field(default = datetime.now())
+    created_at: datetime
 
 
 @app.post("/clients")
 def create_client(client: Client):
-    return client
+    clients.append(client)
+    return clients
 
-@app.get("/")
-def read_root():
-    return {"message": "FastAPI hello world"}
 
+@app.get("/clients")
+def read_clients():
+    return clients
