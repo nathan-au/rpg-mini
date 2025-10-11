2ce2f43ab03a8960ff151d5445a1733af0d8b266
nathan-au <im.nathanau@gmail.com>
2025-10-11 13:48:42 -0400

    implement sqlmodel db

--- main.py
+++ main.py
@@ -2,30 +2,35 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from enum import Enum
 from datetime import datetime
+from sqlmodel import SQLModel, Field, create_engine, Session, select
 
 app = FastAPI()
-
-clients = []
 
 class ComplexityEnum(str, Enum):
     simple = "simple"
     average = "average"
     complex = "complex"
 
-class Client(BaseModel):
-    id: int
+class Client(SQLModel, table = True):
+    id: int | None = Field(default=None, primary_key=True)    
     name: str
     email: str
     complexity: ComplexityEnum
-    created_at: datetime
+    created_at: datetime = Field(default_factory=datetime.now)
 
+engine = create_engine("sqlite:///database.db")
+SQLModel.metadata.create_all(engine)
 
 @app.post("/clients")
 def create_client(client: Client):
-    clients.append(client)
-    return clients
-
+    with Session(engine) as session:
+        session.add(client)
+        session.commit()
+        session.refresh(client)
+        return client
 
 @app.get("/clients")
 def read_clients():
-    return clients
+    with Session(engine) as session:
+        clients_in_db = session.exec(select(Client)).all()
+        return clients_in_db
