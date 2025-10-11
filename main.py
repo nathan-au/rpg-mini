a022054fda8b899a72a36d54a7900db19de83bf9
nathan-au <im.nathanau@gmail.com>
2025-10-11 14:30:26 -0400

    intake with client complexity checklist

--- main.py
+++ main.py
@@ -22,12 +22,34 @@ class IntakeStatusEnum(str, Enum):
     open = "open"
     done = "done"
 
+class ChecklistItemDocKindEnum(str, Enum):
+    T4 = "T4"
+    receipt = "receipt"
+    id = "id"
+class ChecklistItemStatusEnum(str, Enum):
+    missing = "missing"
+    received = "received"
+
+class ChecklistItem(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    intake_id: int = Field(foreign_key="intake.id")
+    doc_kind: ChecklistItemDocKindEnum
+    status: ChecklistItemStatusEnum = Field(default=ChecklistItemStatusEnum.missing)
+    created_at: datetime = Field(default_factory=datetime.now)
+
+CLIENT_COMPLEXITY_CHECKLIST = {
+    "simple": [ChecklistItemDocKindEnum.T4, ChecklistItemDocKindEnum.id],
+    "average": [ChecklistItemDocKindEnum.T4, ChecklistItemDocKindEnum.id, ChecklistItemDocKindEnum.receipt, ChecklistItemDocKindEnum.receipt],
+    "complex": [ChecklistItemDocKindEnum.T4, ChecklistItemDocKindEnum.id, ChecklistItemDocKindEnum.receipt, ChecklistItemDocKindEnum.receipt, ChecklistItemDocKindEnum.receipt, ChecklistItemDocKindEnum.receipt, ChecklistItemDocKindEnum.receipt]
+}
+
 class Intake(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     client_id: int = Field(foreign_key="client.id")  # link to Client
     fiscal_year: int
     status: IntakeStatusEnum = Field(default=IntakeStatusEnum.open)
     created_at: datetime = Field(default_factory=datetime.now)
+
 
 
 engine = create_engine("sqlite:///database.db")
@@ -54,7 +76,6 @@ def create_client(client_data: ClientCreate):
 @app.post("/intakes")
 def create_intake(intake_data: IntakeCreate):
     intake = Intake(**intake_data.model_dump())
-
     with Session(engine) as session:
         client = session.get(Client, intake.client_id)
         if not client:
@@ -62,8 +83,29 @@ def create_intake(intake_data: IntakeCreate):
         session.add(intake)
         session.commit()
         session.refresh(intake)
-        return intake
+        expected_docs = CLIENT_COMPLEXITY_CHECKLIST[client.complexity.value]
 
+        checklist_items = [
+            ChecklistItem(intake_id=intake.id, doc_kind=doc)
+            for doc in expected_docs
+        ]
+
+        session.add_all(checklist_items)
+        session.commit()
+
+        return {
+            "intake": {
+                "id": intake.id,
+                "client_id": intake.client_id,
+                "fiscal_year": intake.fiscal_year,
+                "status": intake.status,
+                "created_at": intake.created_at,
+            },
+            "checklist": [
+                {"doc_kind": item.doc_kind, "status": item.status}
+                for item in checklist_items
+            ],
+        }
 @app.get("/clients")
 def read_clients():
     with Session(engine) as session:
